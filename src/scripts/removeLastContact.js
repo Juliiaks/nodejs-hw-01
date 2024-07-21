ba933e1541408a8ef251d739be41ab32ed4ca467
Juliiaks <juliakharetonchuk@gmail.com>
2024-07-21 18:01:17 +0300

    add 6, 7

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,17 @@
-import { PATH_DB } from '../constants/contacts.js';
 
-export const removeLastContact = async () => {};
+import { PATH_DB } from '../constants/contacts.js';
+import fs from "node:fs/promises";
+
+export const removeLastContact = async () => {
+    try {
+        const data = await fs.readFile(PATH_DB, 'utf8');
+        const parsedData = JSON.parse(data);
+        parsedData.pop();
+
+        await fs.writeFile(PATH_DB, JSON.stringify(parsedData, null, 2));
+    } catch(error) {
+        console.log(error);
+    }
+};
 
 removeLastContact();
