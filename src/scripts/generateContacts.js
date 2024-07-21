cefb805fa28c8964ac32869524f4a3bea8ce6a06
Juliiaks <juliakharetonchuk@gmail.com>
2024-07-21 15:48:59 +0300

    add 4 tasks

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,19 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+import fs from "node:fs/promises";
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+    try {
+        const data = await fs.readFile(PATH_DB, 'utf8');
+        const parsedData = JSON.parse(data);
+        for (let i = 1; i <= number; i++){
+            const contact = createFakeContact();
+            parsedData.push(contact);
+        }
+        fs.writeFile(PATH_DB, JSON.stringify(parsedData, null, 2));
+    } catch (error) {
+console.log(error);
+    }
+};
 
 generateContacts(5);
